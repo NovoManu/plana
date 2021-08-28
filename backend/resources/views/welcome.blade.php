acecddd61d943b317930481c1eac3e9edfa6850d
Manu <operamike@mail.ru>
2021-08-28 20:04:31 +0200

    feat: Init and added laravel project

---
+++ backend/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Service API</title>
+    </head>
+    <body>
+        <div>API Service</div>
+        <div>Check API documentation <a href="/api/docs">here</a></div>
+    </body>
+</html>
